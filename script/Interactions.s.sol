e70ce8ae8ea9bdf7130e6a6428852639b518a2cd
Aryan Godara <aryangodara03@gmail.com>
2023-11-07 13:57:57 +0530

    add fundSubscriber function
    Signed-off-by: Aryan Godara <aryangodara03@gmail.com>

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
 import {VRFCoordinatorV2Mock} from "@chainlink/contracts/src/v0.8/mocks/VRFCoordinatorV2Mock.sol";
+import {LinkToken} from "../test/mocks/LinkToken.sol";
 
 contract CreateSubscription is Script {
     function CreateSubscriptionUsingConfig() public returns (uint64) {
@@ -33,9 +34,25 @@ contract FundSubscription is Script {
 
     function fundSubscriptionUsingConfig() public {
         HelperConfig helperConfig = new HelperConfig();
-        (,,address vrfCoordinator,,uint64 subId,,) = helperConfig.activeNetworkConfig();
+        (,,address vrfCoordinator,,uint64 subId,,address link) = helperConfig.activeNetworkConfig();
 
+        fundSubscription(vrfCoordinator, subId, link);
+    }
 
+    function fundSubscription (address vrfCooordinator, uint64 subId, address link) public {
+        console.log("Funding subscription: ", subId);
+        console.log("Using vrfCoordinator: ", vrfCooordinator);
+        console.log("On ChainID: ", block.chainid);
+
+        if (block.chainid == 31337 ) {
+            vm.startBroadcast();
+            VRFCoordinatorV2Mock(vrfCooordinator).fundSubscription(subId, FUND_AMOUNT);
+            vm.stopBroadcast();
+        } else {
+            vm.startBroadcast();
+            LinkToken(link).transferAndCall(vrfCooordinator, FUND_AMOUNT, abi.encode(subId));
+            vm.stopBroadcast();
+        }
     }
 
     function run() external {
